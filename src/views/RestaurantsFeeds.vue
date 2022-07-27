67f7fc8d0085cb9c71d1f991a421dccf72e3f445
unknown <chacha0238@gmail.com>
2022-07-27 12:07:36 +0800

    spinner

--- src/views/RestaurantsFeeds.vue
+++ src/views/RestaurantsFeeds.vue
@@ -1,26 +1,30 @@
 <template>
   <div class="container py-5">
     <NavTabs />
-    <h1 class="mt-5">最新動態</h1>
-    <hr />
-    <div class="row">
-      <div class="col-md-6">
-        <h3>最新餐廳</h3>
-        <!-- 最新餐廳 NewestRestaurants -->
-        <NewestRestaurants :restaurants="restaurants" />
+    <Spinner v-if="isLoading" />
+    <template v-else>
+      <h1 class="mt-5">最新動態</h1>
+      <hr />
+      <div class="row">
+        <div class="col-md-6">
+          <h3>最新餐廳</h3>
+          <!-- 最新餐廳 NewestRestaurants -->
+          <NewestRestaurants :restaurants="restaurants" />
+        </div>
+        <div class="col-md-6">
+          <h3>最新評論</h3>
+          <!-- 最新評論 NewestComments-->
+          <NewestComments :comments="comments" />
+        </div>
       </div>
-      <div class="col-md-6">
-        <h3>最新評論</h3>
-        <!-- 最新評論 NewestComments-->
-        <NewestComments :comments="comments" />
-      </div>
-    </div>
+    </template>
   </div>
 </template>
 <script>
 import NavTabs from "./../components/NavTabs.vue";
 import NewestRestaurants from "./../components/NewestRestaurants.vue";
 import NewestComments from "./../components/NewestComments.vue";
+import Spinner from "./../components/Spinner.vue";
 import restaurantsAPI from "./../apis/restaurants";
 import { Toast } from "./../utils/helpers";
 
@@ -29,11 +33,13 @@ export default {
     NavTabs,
     NewestRestaurants,
     NewestComments,
+    Spinner,
   },
   data() {
     return {
       restaurants: [],
       comments: [],
+      isLoading: true,
     };
   },
   created() {
@@ -48,7 +54,9 @@ export default {
         this.comments = comments.filter(
           (comment) => comment.Restaurant && comment.text
         );
+        this.isLoading = false;
       } catch (error) {
+        this.isLoading = false;
         Toast.fire({
           icon: "error",
           title: "無法取得最新動態，請稍後再試",
